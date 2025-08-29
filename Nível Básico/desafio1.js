b32230a18c84bcd068fbf19962fa78f2366f3bfb
Ana <anar.barontini@gmail.com>
2025-08-29 17:35:16 -0300

    Desafios JS

---
+++ Nível Básico/desafio1.js
@@ -0,0 +1,13 @@
+console.log("Bem vindo!");
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Digite o seu nome:");
+console.log(`Seu nome é: ${nome}`);
+
+let idade = prompt("Digite sua idade: ");
+
+if(idade >=18){
+    console.log("Você é maior de idade!")
+}else{
+    console.log("Você não é maior de idade!")
+}
